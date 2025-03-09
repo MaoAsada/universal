8652a4fe821676d7f5ac30dd7268fcc9325bd992
MaoAsada <133876702+MaoAsada@users.noreply.github.com>
2025-03-09 12:54:51 +0800

    Update maomusic.js

--- maomusic.js
+++ maomusic.js
@@ -19,7 +19,7 @@ if (body) {
       // 将歌手改为固定文本“点击播放—>”
       obj.data.song_singer = "Mao的定制电台";
       // 修改封面为指定链接
-      obj.data.cover = "https://github.com/MaoAsada/universal/blob/main/profile_photo.jpg";
+      obj.data.cover = "https://raw.githubusercontent.com/MaoAsada/universal/refs/heads/main/profile_photo.jpg";
     }
     $done({body: JSON.stringify(obj)});
   } catch (e) {
