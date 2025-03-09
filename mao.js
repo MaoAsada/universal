d58d246d80c484d6265ee2bfa381957e6f622c97
MaoAsada <133876702+MaoAsada@users.noreply.github.com>
2025-03-09 12:56:14 +0800

    Update mao.js

--- mao.js
+++ mao.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/MaoAsada/universal/refs/heads/main/maomusic.js?token=GHSAT0AAAAAADAGDTZ7CWCDS5IHZ6EGENPQZ6NDR5A
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/MaoAsada/universal/refs/heads/main/maomusic.js
 
 [mitm] 
 hostname = api.dragonlongzhu.cn
